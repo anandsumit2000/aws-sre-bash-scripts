5b414a00334165cb46819dacdf0b26c570da77a3
Sumit Anand <anandsumit2000@gmail.com>
2024-02-13 21:04:23 +0530

    Passing an array of host IPs and key paths to loop

--- instance_reachability_check/instance_reachability.sh
+++ instance_reachability_check/instance_reachability.sh
@@ -7,7 +7,15 @@ check_instance_health() {
         ssh -i "$key_file" ec2-user@"$instance_ip" 'echo "Host Reachable." || echo "Host Unreachable"'
 }
 
-aws_instance_ip="<host private IP>"
-key_file="<path to key_file.pem>"
+# Specify private IPs of Hosts
+aws_instance_ips=("host1" "host2" "host3")
 
-check_instance_health "$aws_instance_ip" "$key_file"
+# Specify the complete path to the respective keys to the hosts in same order
+key_files=("host1" "host2" "host3")
+
+
+for i in {1..${#aws_instance_ips[@]}}; do
+    aws_instance_ip="${aws_instance_ips[$i]}"
+    key_file="${key_files[$i]}"
+    check_instance_health "$aws_instance_ip" "$key_file"
+done
